2c6592281291f5bf6faf44dda8c16035f236c46b
Jesus <r.jesus9107@gmail.com>
2022-02-09 19:00:41 -0800

    Design corrections

--- src/components/main.component.jsx
+++ src/components/main.component.jsx
@@ -17,7 +17,7 @@ const Main = () => {
     
     return (
         <main className={`content min-h-[550px]`}>
-            <nav className="px-6 sm:mx-auto sm:w-2/3 flex justify-around sm:justify-between py-8 uppercase tracking-widest z-10">
+            <nav className="bg-neutral-100 px-6 sm:mx-auto sm:w-2/3 flex justify-around sm:justify-between py-8 uppercase tracking-widest z-10">
             <Link to="/" className={`${(currentLocation === "/" && "border-[#E2C67F]") || "border-b-transparent"} location`}>home</Link>
             <Link to="/projects" className={`${(currentLocation === "/projects" && "border-[#E2C67F]") || "border-b-transparent"} location`}>projects</Link>
             <Link to="/about" className={`${(currentLocation === "/about" && "border-[#E2C67F]") || "border-b-transparent"} location`}>about</Link>
