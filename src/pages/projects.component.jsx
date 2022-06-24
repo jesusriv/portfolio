c86372a15042df60140cd2ed92123a2fc144c1cd
jesusriv <r.jesus9107@gmail.com>
2022-06-24 09:46:19 -0700

    center projects for larger screens

--- src/pages/projects.component.jsx
+++ src/pages/projects.component.jsx
@@ -7,11 +7,11 @@ const Projects = () => {
     return (
         <div className="relative" id='projects'>
             <div className='w-full h-full flex justify-start py-20 px-10
-                space-x-8 srollbar-default overflow-x-auto lg:justify-center'>
+                space-x-8 srollbar-default overflow-x-auto md:space-x-0 lg:justify-evenly'>
                 {projects && projects.map((project, i) => {
                     return <Project key={i} project={project} />;
                 })}
-                <div className='w-10 h-full border-2 border-gray-100'></div>
+                <div className='w-10 h-full border-2 border-gray-100 md:hidden'></div>
             </div>
         </div>
     );
